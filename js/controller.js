535bc7d6ec73b3ef597e2ea05c380d59cd364a55
Jisoo Yoon <jyoon006@gmail.com>
2016-01-24 20:06:41 -0500

    tried to change controller to make it work with chrome browser. researched online what I can change, but still doesn't work

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,14 @@
-var myApp = angular.module('myBlog', []);
+var myApp = angular.module('myBlog', [])
+	.config(['$httpProvider', function($httpProvider) {
+        delete $httpProvider.defaults.headers.common["X-Requested-With"]
+    }]);
+
 
 myApp.controller('myEntries', function($scope, $http) {
+	$http.defaults.useXDomain = true;
 	$scope.blogs = [];
 	
 	$http.get('js/data.json').success(function(data) {
 		$scope.blogs = data;
-	});
+		});
 });
